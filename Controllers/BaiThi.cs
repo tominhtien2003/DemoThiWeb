61d72eea2e32d3e7b4d5305f4da272762c4efd1b
Tô Minh Tiến <totiendepzai@gmail.com>
2023-10-28 20:10:51 +0700

    update

---
+++ Controllers/BaiThi.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoThiWeb.Controllers
+{
+    public class BaiThi : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
