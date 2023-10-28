77eb0cedca10c9cc959f6f258ce01d7cf448f37d
Tô Minh Tiến <totiendepzai@gmail.com>
2023-10-28 18:55:51 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DemoThiWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
